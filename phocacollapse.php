4fced7c006f3985f8d87b6be58d0c66a6ed56183
no64 <no64@users.noreply.github.com>
2026-07-27 09:47:02 +0200

    Update phocacollapse.php
    - Added options for 
    - - icon style
    - - preserve Collapse state
    - - etc.
    
    - Added collapse button to 
    - - categoryList
    - - tagList
    - - fieldList
    - - contactList
    
    - fixed menuitemList (old was 'itemList'

--- phocacollapse.php
+++ phocacollapse.php
@@ -7,99 +7,218 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
  */
 
-
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Plugin\CMSPlugin;
 
-defined( '_JEXEC' ) or die( 'Restricted access' );
-jimport( 'joomla.plugin.plugin' );
+defined('_JEXEC') or die('Restricted access');
+
+jimport('joomla.plugin.plugin');
 
 class plgSystemPhocaCollapse extends CMSPlugin
 {
 	protected $autoloadLanguage = true;
 
-	public function __construct(& $subject, $config) {
+	public function __construct(&$subject, $config)
+	{
 		$this->loadLanguage('plg_system_phocacollapse');
 		parent::__construct($subject, $config);
 	}
 
-	function onBeforeRender() {
+	public function onBeforeRender()
+	{
+		$app = Factory::getApplication();
 
-		$app 	= Factory::getApplication();
 		if ($app->getDocument()->getType() !== 'html') {
 			return true;
 		}
 
-		$frontend_active 		= $this->params->get('frontend_active', 0);
+		$frontend_active = $this->params->get('frontend_active', 0);
+
 		if ($frontend_active == 0 && !Factory::getApplication()->isClient('administrator')) {
 			return true;
 		}
 
 		$wa = $app->getDocument()->getWebAssetManager();
-		$wa->registerAndUseScript('plg_system_phocacollapse.phocacollapse', 'media/plg_system_phocacollapse/js/phocacollapse.es6.js', array('version' => 'auto'));
-		$wa->registerAndUseStyle('plg_system_phocacollapse.phocacollapse', 'media/plg_system_phocacollapse/css/phocacollapse.css', array('version' => 'auto'));
 
+		$wa->registerAndUseScript(
+			'plg_system_phocacollapse.phocacollapse',
+			'media/plg_system_phocacollapse/js/phocacollapse.es6.js',
+			['version' => 'auto']
+		);
+
+		$wa->registerAndUseStyle(
+			'plg_system_phocacollapse.phocacollapse',
+			'media/plg_system_phocacollapse/css/phocacollapse.css',
+			['version' => 'auto']
+		);
+
+
+		$wa->addInlineScript(
+			'window.phocaCollapseOptions = ' . json_encode([
+				'iconStyle'           	=> $this->params->get('icon_style', 'eye'),
+				'individualCollapse'  	=> (int) $this->params->get('individual_collapse', 0),
+				'preserveCollapseState' => (int) $this->params->get('preserve_collapse_state', 0),
+				
+				'expandText'          => Text::_('PLG_SYSTEM_PHOCACOLLAPSE_EXPAND'),
+				'collapseText'        => Text::_('PLG_SYSTEM_PHOCACOLLAPSE_COLLAPSE'),
+				
+				'expandAllText'       => Text::_('PLG_SYSTEM_PHOCACOLLAPSE_EXPAND_ALL'),
+				'collapseAllText'     => Text::_('PLG_SYSTEM_PHOCACOLLAPSE_COLLAPSE_ALL'),		
+			]) . ';'
+		);
+
+
+		return true;
 	}
 
-	function onAfterRender() {
-
+	public function onAfterRender()
+	{
 		$app = Factory::getApplication();
+
 		if ($app->getDocument()->getType() !== 'html') {
 			return true;
 		}
-		$frontend_active 		= $this->params->get('frontend_active', 0);
+
+		$frontend_active = $this->params->get('frontend_active', 0);
+
 		if ($frontend_active == 0 && !Factory::getApplication()->isClient('administrator')) {
 			return true;
 		}
 
-		$text_format 		= $this->params->get('text_format', 1);
+		$text_format = $this->params->get('text_format', 1);
+		$icon_style  = $this->params->get('icon_style', 'eye');
 
-		$text = Text::_('PLG_SYSTEM_PHOCACOLLAPSE_SHOW_HIDE');
+		
+		$text = Text::_('PLG_SYSTEM_PHOCACOLLAPSE_COLLAPSE_ALL');
+	
+		$class = 'btn btn-sm btn-secondary phCollapseClick';
 
-		$class = 'phCollapseClick';
-		$icon = '<span class="icon-eye" aria-hidden="true"></span>';
+		switch ($icon_style) {
 
-		switch($text_format){
+			case 'chevron':
+				$icon = '<span class="icon-chevron-down" aria-hidden="true"></span>';
+				$iconOnly = '<span class="icon-chevron-down icon-white" aria-hidden="true" style="pointer-events:none;"></span>';
+			break;
+
+			case 'eye':
+			default:
+				$icon = '<span class="icon-eye" aria-hidden="true"></span>';
+				$iconOnly = '<span class="icon-eye icon-white" aria-hidden="true" style="pointer-events:none;"></span>';
+			break;
+		}
+
+		switch ($text_format) {
 
 			case 2:
-				//$text = $text;
+				// Text only
+				$text = '<span class="ph-collapse-text">'
+					. Text::_('PLG_SYSTEM_PHOCACOLLAPSE_COLLAPSE_ALL')
+					. '</span>';
 			break;
 
 			case 3:
-				$text = '<span class="icon-eye icon-white" aria-hidden="true" style="pointer-events: none;"></span>';
-				$class = 'group-show btn btn-primary btn-sm phCollapseClick';
+				// Icon only
+				$text = $iconOnly;
+				$class = 'group-show btn btn-sm btn-secondary phCollapseClick';
 			break;
 
 			case 1:
 			default:
-				$text = $icon . ' ' . $text;
+				// Icon + Text
+				$text = $icon . ' <span class="ph-collapse-text">'
+					. Text::_('PLG_SYSTEM_PHOCACOLLAPSE_COLLAPSE_ALL')
+					. '</span>';
+					
 			break;
 		}
-
-
 
 		$buffer = $app->getBody();
 
 		$from = '<div class="subform-repeatable-wrapper subform-layout">';
-		$to   = $from . '<div class="ph-collapse"><a href="javascript:void(0)" class="'.$class.'" data-name="phCollapseClick">' . $text . '</a></div>';
 
+		$to = $from
+			. '<div class="ph-collapse">'
+			. '<a href="javascript:void(0)" class="' . $class . '" data-name="phCollapseClick">'
+			. $text
+			. '</a></div>';
+		
 		$buffer = str_replace($from, $to, $buffer);
 
-		$from = '<table class="table" id="itemList">';
-		$to   = $from . '<div class="ph-collapse"><a href="javascript:void(0)" class="'.$class.'" data-name="phCollapseClickMenu">' . $text . '</a></div>';
 
-		$buffer = str_replace($from, $to, $buffer);
+
 
 
 		$from = '<table class="table itemList" id="articleList">';
-		$to   = $from . '<div class="ph-collapse"><a href="javascript:void(0)" class="'.$class.'" data-name="phCollapseClickArticle">' . $text . '</a></div>';
+		$to   = $from
+			. '<div class="ph-collapse">'
+			. '<a href="javascript:void(0)" class="' . $class . '" data-name="phCollapseClickArticle">'
+			. $text
+			. '</a></div>';
+
+		$buffer = str_replace($from, $to, $buffer);
+		
+		
+$from = '<table class="table" id="menuitemList">';
+		$to   = $from
+			. '<div class="ph-collapse">'
+			. '<a href="javascript:void(0)" class="' . $class . '" data-name="phCollapseClickMenu">'
+			. $text
+			. '</a></div>';
 
 		$buffer = str_replace($from, $to, $buffer);
 
 
+
+		
+$from = '<table class="table" id="categoryList">';
+		$to   = $from
+			. '<div class="ph-collapse">'
+			. '<a href="javascript:void(0)" class="' . $class . '" data-name="phCollapseClickMenu">'
+			. $text
+			. '</a></div>';
+
+		$buffer = str_replace($from, $to, $buffer);
+			
+		
+$from = '<table class="table" id="tagList">';
+		$to   = $from
+			. '<div class="ph-collapse">'
+			. '<a href="javascript:void(0)" class="' . $class . '" data-name="phCollapseClickMenu">'
+			. $text
+			. '</a></div>';
+
+		$buffer = str_replace($from, $to, $buffer);		
+						
+		
+
+			
+
+$from = '<table class="table" id="fieldList">';
+		$to   = $from
+			. '<div class="ph-collapse">'
+			. '<a href="javascript:void(0)" class="' . $class . '" data-name="phCollapseClickField">'
+			. $text
+			. '</a></div>';
+
+		$buffer = str_replace($from, $to, $buffer);	
+			
+		
+$from = '<table class="table" id="contactList">';
+		$to   = $from
+			. '<div class="ph-collapse">'
+			. '<a href="javascript:void(0)" class="' . $class . '" data-name="phCollapseClickField">'
+			. $text
+			. '</a></div>';
+
+		$buffer = str_replace($from, $to, $buffer);			
+
+
+		
+
 		$app->setBody($buffer);
+
 		return true;
 	}
 }
-?>
+
